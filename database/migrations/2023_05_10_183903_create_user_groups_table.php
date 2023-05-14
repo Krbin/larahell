bba4c172d8b52d6aca1be6a1e5b718b5a982d44c
Krbin <kubin.krys@gmail.com>
2023-05-14 14:33:23 +0200

    user groups or something

--- database/migrations/2023_05_10_183903_create_user_groups_table.php
+++ database/migrations/2023_05_10_183903_create_user_groups_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_groups', function (Blueprint $table) {
+        Schema::create('user_group', function (Blueprint $table) {
             $table->integer('user_id')->unsigned();
             $table->integer('group_id')->unsigned();
             $table->foreign('user_id')
